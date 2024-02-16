1c239f2231e0301c7ca1ff68dc7cde86ded713be
dhinesh454 <dhinesh454@gmail.com>
2024-02-16 13:21:53 +0530

    Build Header Task1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {Fragment} from "react";
+import Header from "./components/Layout/Header";
+
+
+function App() {
+
+
+  return (
+    <Fragment>
+      <Header/>   
+    </Fragment>
+  )
+
+
+}
+
+export default App;
+
